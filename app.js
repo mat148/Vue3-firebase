fb82399206f399ab1bfca6a8efdd15d6f83d7289
Mathew Morse <mrm1779@gmail.com>
2020-12-12 23:36:57 -0500

    Conditional rendering

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
+            showBooks: true,
             title: 'Lorem ipsum',
             author: 'Lorem',
             age: '0'
         }
     },
     methods: {
-        changeTitle(title) {
+        /*changeTitle(title) {
             this.title = title
+        }*/
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         }
     }
 })
