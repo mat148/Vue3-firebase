98baa0515dbb3ff734cab7bb4c39bf25a076c47d
Mathew Morse <mrm1779@gmail.com>
2020-12-13 23:59:17 -0500

    Lesson 3

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Lesson 2
+# Lesson 3
--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const app = Vue.createApp({
-
+    data() {
+        return {
+            title: 'The Way of Kings',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
 })
 
 app.mount('#app')
