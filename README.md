de0b5cf436e491ce46a83ef2e8368de4cfc559b3
Mathew Morse <mrm1779@gmail.com>
2020-12-19 14:41:01 -0500

    Lesson 7 v-for

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Lesson 6
+# Lesson 7
--- app.js
+++ app.js
@@ -2,26 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Way of Kings',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books : [
+                { title: 'Name of the wind', author: 'Patrick Rothfuss' },
+                { title: 'the way of kings', author: 'brandon sanderson' },
+                { title: 'the final empire', author: 'brandon sanderson' }
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks =  !this.showBooks
-        },
-        handleEvent(e, data) {
-            console.log(e, e.type);
-            if(data){
-                console.log(data);
-            }
-        },
-        handleMouseMove(e) {
-            this.x = e.offsetX;
-            this.y = e.offsetY;
         }
     }
 })
