eeb4eb64629a76342ab399bc330aafca27faa2ee
Mathew Morse <mrm1779@gmail.com>
2020-12-21 15:54:29 -0500

    Lesson-15: Multiple components

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+  <Modal/>
 </template>
 
 <script>
+import Modal from './components/Modal'
+
 export default {
   name: 'App',
+  components: {
+    Modal
+  },
   data() {
     return {
       title: "Baby's first Vue app"
