e20cdefe970b027eab13a190c61b8dfb3e6d974f
Mathew Morse <mrm1779@gmail.com>
2020-12-21 16:04:21 -0500

    Lesson-16: Component styles & global styles

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,24 +1,31 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
+    <div class="modal-backdrop">
+        <div class="modal-item">
+            <h1>Modal Title</h1>
             <p>Modal Content</p>
         </div>
     </div>
 </template>
 
+<!-- <style scoped> --> 
 <style>
-    .modal {
+    .modal-item {
         width: 400px;
         padding: 20px;
         margin: 100px auto;
         background: white;
         border-radius: 10px;
     }
-    .backdrop {
+    .modal-backdrop {
         top: 0;
         position: fixed;
         background: rgba(0,0,0,0.5);
         width: 100%;
         height: 100%;
+    }
+    .modal-item h1 {
+        color: red;
+        border: none;
+        padding: 0;
     }
 </style>
